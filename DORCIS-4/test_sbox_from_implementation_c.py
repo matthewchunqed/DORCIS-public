4b6911bcb1b1d9fd1b856f5137e924c3d910d757
matthewchunqed <108086692+matthewchunqed@users.noreply.github.com>
2023-05-19 01:55:48 -0400

    Add files via upload

--- DORCIS-4/test_sbox_from_implementation_c.py
+++ DORCIS-4/test_sbox_from_implementation_c.py
@@ -10,19 +10,16 @@ def CCNOT2(a, b, c):
 	return (a & b) ^ c
 	#return a ^ (b & c)
 
-def CCCNOT2(a, b, c, d):
-	return (a & b & c) ^ d
-
 Z = []
-for f in range(16)[::]:
+for f in range(8)[::]:
 
-	Y = [None]*4
+	Y = [None]*3
 
 
-	X = [x == '1' for x in list((bin(f)[2:]).zfill(4))]
-	F = [None]*4
+	X = [x == '1' for x in list((bin(f)[2:]).zfill(3))]
+	F = [None]*3
 
-	############ PRINCE SBox (BF32AC916780E5D4)
+	############ Twine SBox (C0FA2B9583D71E64)
     ########### Copy starts
 	F[0] = X[3];
 	F[1] = X[2];
@@ -55,7 +52,7 @@ for f in range(16)[::]:
 	Z.append(Y)
 	
 
-for f in range(16):
+for f in range(8):
 	if f not in Z:
 		print ('Error, missing %x'%f)
 print((''.join(map(lambda x: hex(x)[2:], Z))).upper())
